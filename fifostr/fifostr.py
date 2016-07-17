a0f1703383a4ba838aface9d25da3591f5d929b9
Manu Chatterjee <manu@deftio.com>
2016-07-16 23:44:49 -0700

    added __init__.py to make fifostr a proper python module

--- fifostr/fifostr.py
+++ fifostr/fifostr.py
@@ -682,6 +682,6 @@ class fifostr(deque):
 			dict: containing current version
 		"""
 		return 	{
-					"version" : [1,0,4]					
+					"version" : [1,0,5]					
 				}
 
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.4',
+    version='1.0.5',
  
     description='fifostr - A FIFO (first in first out) buffer for strings derived from deque with pattern match callbacks',
     long_description=long_description,
