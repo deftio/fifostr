ca093ae0393ede435aa1fb5cc500e30c7ab428f1
Manu Chatterjee <deftio@deftio.com>
2018-11-09 21:38:22 -0800

    updated index.html autogen

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
-    # $ pip install -e .[dev,test]
+    # $ pip install -e .[dev,test] 
     extras_require={
         'dev': ['check-manifest'],
         'test': ['pytest']
